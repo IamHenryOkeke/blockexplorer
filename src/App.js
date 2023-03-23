a502199e206e727edeaa2d741698b191fbc92c4c
Henry Okeke <iamhenryokeke@gmail.com>
2023-03-23 21:26:02 +0100

    added basic functionality and states for displaying response object

--- src/App.js
+++ src/App.js
@@ -19,18 +19,56 @@ const settings = {
 //   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
-function App() {
-  const [blockNumber, setBlockNumber] = useState();
-
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+const App = () => {
+  const [blockNumber, setBlockNumber] = useState("");
+  const [nonce, setNonce] = useState("");
+  const [hash, setHash] = useState("");
+  const [gasLimit, setGasLimit] = useState("");
+  const [gasUsed, setGasUsed] = useState("");
+  const [numberOfTxns, setNumberOfTxns] = useState("");
+  const [timeStamp, setTimeStamp] = useState("");
+  // function to add commas to figures
+  function numberWithCommas(number){
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+useEffect(() => {
+    const getBlockNumber = async () => {
+      const newBlockNumber = await alchemy.core.getBlockNumber();
+      const blockTxns = await alchemy.core.getBlockWithTransactions(newBlockNumber);
+      console.log(blockTxns)
+      
+      setBlockNumber(blockTxns.number);
+      setHash(blockTxns.hash);
+      setGasLimit(parseInt(blockTxns.gasLimit._hex, 16));
+      setGasUsed(parseInt(blockTxns.gasUsed._hex, 16));
+      setNonce(blockTxns.nonce);
+      setNumberOfTxns((blockTxns.transactions).length)
+      setTimeStamp(new Date(blockTxns.timestamp * 1000))
     }
 
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (
+    <div className="App">
+      <div>
+        <input type="text" placeholder='Enter block number' />
+        <button>Search</button>
+      </div>
+      <div className="block-details">
+        <p>Block Number: {blockNumber}</p>
+        <p>Hash: {hash}</p> 
+        <p>Nonce: {nonce}</p>
+        <p>Gas used: {numberWithCommas(gasUsed)} ({((gasUsed/gasLimit) * 100).toFixed(2)}% gas used)</p>
+        <p>Gas Limit: {numberWithCommas(gasLimit)}</p>
+        <p>{numberOfTxns} transactions in this block</p>
+        <p>Timestamp:  {`${timeStamp}`}</p>
+        {/* <p>{blockNumber}</p>
+        <p>{blockNumber}</p>
+        <p>{blockNumber}</p> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
