60cebe695f83f56ee4b0dedbfa0504196a61c7fb
Henry Okeke <iamhenryokeke@gmail.com>
2023-03-31 14:53:16 +0100

    added link to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = () => {
 
     return (
         <div className="navbar flex justify-between text-lg">
-            <h1 className="logo text-[#6b6869]">EthExplorer</h1>
+            <a href="/blockexplorer">
+                <h1 className="logo text-[#6b6869]">EthExplorer</h1>
+            </a>
             <div className="links flex gap-2 md:gap-5 lg:gap-7 text-[#a07281]">
                 <Button variant='outlined'>Home</Button>
                 <Button variant='outlined'>Contact</Button>
