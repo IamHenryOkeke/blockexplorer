bef756bb2de00de1cff9eb2eb3551bf1bd7a2f8e
Henry Okeke <iamhenryokeke@gmail.com>
2023-03-31 14:34:57 +0100

    removed link tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,8 @@ const Navbar = () => {
         <div className="navbar flex justify-between text-lg">
             <h1 className="logo text-[#6b6869]">EthExplorer</h1>
             <div className="links flex gap-2 md:gap-5 lg:gap-7 text-[#a07281]">
-                <a href="/">
-                    <Button variant='outlined'>Home</Button>
-                </a>
-                <a href="/">
+                <Button variant='outlined'>Home</Button>
                 <Button variant='outlined'>Contact</Button>
-                </a>
             </div>
         </div>
     )
