3a988a4a9f99fdfed874d412b4e3878be4a62511
Henry Okeke <iamhenryokeke@gmail.com>
2023-04-01 22:08:20 +0100

    added logic for pagination functionality

--- src/components/TransactionDetails.js
+++ src/components/TransactionDetails.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ReactPaginate from 'react-paginate';
 
-const transactionDetails = ({ data }) => {
+const TransactionDetails = ({ data }) => {
+
+  const [currentItems, setCurrentItems] = useState([]);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+
+  const itemsPerPage = 10
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(data.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(data.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, data])
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className='transaction-container flex flex-col gap-2'>
-      {data.map((item, index) => {
-        return (
-          <div className="transaction border border-[#ceb9bf] rounded py-2 px-1" key={index} id={index}>
-            <p className='break-words'>Transaction Hash: {item.hash}</p>
-            <p className='break-words'>From: {item.from}</p>
-            <p className='break-words'>To: {item.to}</p>
-            <p>Value: {(parseInt(item.value._hex, 16)) / 10 ** 18} Eth</p>
-          </div>
-        )
-      })}
-    </div>
-  )
+    <>
+      <div className='transaction-container flex flex-col gap-2'>
+        {currentItems.map((item, index) => {
+          return (
+            <div className="transaction border border-[#ceb9bf] rounded py-2 px-1" key={index} id={index}>
+              <p className='break-words'>Transaction Hash: {item.hash}</p>
+              <p className='break-words'>From: {item.from}</p>
+              <p className='break-words'>To: {item.to}</p>
+              <p>Value: {(parseInt(item.value._hex, 16)) / 10 ** 18} Eth</p>
+            </div>
+          )
+        })}
+      </div>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName = "pagination list-none flex justify-center items-center gap-1 m-5 text-sm"
+        pageLinkClassName='page-num px-[15px] py-[8px] cursor-pointer rounded-[3px] font-normal hover:bg-gray-500'
+        previousLinkClassName='page-num px-2 py-1 bg-blue-500 rounded-[5px]'
+        nextLinkClassName='page-num bg-blue-500 px-2 py-1 bg-blue-500 rounded-[5px]'
+        activeLinkClassName='active px-2 py-1 bg-blue-500 rounded-[5px]'
+      />
+    </>
+  );
 }
 
-export default transactionDetails
+export default TransactionDetails
