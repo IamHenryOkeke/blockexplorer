4ac5b9fb55c9208e143420795dd034a61418c13d
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-02 12:20:35 +0100

    added socials link and finished contact page styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 
 const Contact = () => {
@@ -8,8 +8,24 @@ const Contact = () => {
 
   return (
     <div className='flex flex-col items-center mx-3'>
-      <h1 className='text-2xl md:text-4xl'>Get in touch</h1>
-      <div className='flex flex-col gap-4 w-full md:w-[60%]'>
+      <h1 className='text-2xl md:text-4xl mb-5'>Get in touch</h1>
+      <p className='text-xl md:text-2xl mb-2'>My Socials</p>
+      <div className='flex gap-5 mb-5'>
+        <a href="https://twitter.com/NmesomaHenry">
+          <svg height="25px" width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>twitter</title><path d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" /></svg>
+        </a>
+        <a href="https://github.com/IamHenryOkeke">
+          <svg height="25px" width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>github</title><path d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>
+        </a>
+        <a href="http://www.facebook.com/henry.okeke.1000">
+          <svg height="25px" width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>facebook</title><path d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" /></svg>
+        </a>
+        <a href="tel:+2347080194374">
+          <svg className='animate-vibration' height="25px" width="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>phone</title><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
+        </a>
+      </div>
+      <div height="25px" width="25px" className='flex flex-col gap-4 w-full md:w-[60%]'>
+        <h1 className='text-xl md:text-2xl -mb-3'>Send a mail</h1>
         <label>
           Message Subject:
         </label>
@@ -17,12 +33,12 @@ const Contact = () => {
           type="text"
           value={subject}
           onChange={(e) => setSubject(e.target.value)}
-          className='border-2 border-black rounded-md p-2.5'
+          className='border-2 border-black rounded-md p-2'
         />
 
         <label>Your message</label>
         <textarea rows="5" value={message}
-          onChange={(e) => setMessage(e.target.value)} class="p-2.5 border-2 border-black text-gray-900 bg-gray-50 resize-none rounded-md" placeholder="Write your message here..."></textarea>
+          onChange={(e) => setMessage(e.target.value)} class="p-2 border-2 border-black text-gray-900 bg-gray-50 resize-none rounded-md" placeholder="Write your message here..."></textarea>
         <a href={`mailto:iamhenryokeke@gmail.com?subject=${subject}&body=${message}`}>
           <button className='px-4 py-1 bg-slate-400 border rounded-md'>Click to Send an Email</button>
         </a>
