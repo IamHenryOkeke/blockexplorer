e8b8a92a08f6741e8464ef6e406e63b8bab2e6b6
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-02 12:22:54 +0100

    added config for phone vibration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily : {
-        body:['Jost']
+      fontFamily: {
+        body: ['Jost']
+      },
+      keyframes: {
+        vibration: {
+          '0%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(15deg)' },
+          '50%': { transform: 'rotate(0deg)' },
+          '75%': { transform: 'rotate(-15deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        }
+      },
+      animation: {
+        vibration: 'vibration 2s ease-in infinite',
       }
     },
   },
